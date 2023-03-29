26e2c09a25a4a7ac62c8fb182b31fe20b8235e71
yehiametwally55 <yehiametwally55@gmail.com>
2023-03-29 19:55:59 +0200

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { PortofolioComponent } from './portofolio/portofolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: HomeComponent },
+  { path: 'Portofolio', component: PortofolioComponent },
+  { path: 'About', component: AboutComponent },
+  { path: 'Contact-Us', component: ContactUsComponent },
+
+  { path: '**', component: HomeComponent }, 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { PortofolioComponent } from './portofolio/portofolio.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    PortofolioComponent,
+    ContactUsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
